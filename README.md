0c03e74c3e5186122df0eabb6969edf150014b01
Gustan <skhafabii@gmail.com>
2023-04-13 10:57:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# **IMPORTANT**
+
+This repository is not being maintained anymore. The code was split into two new repositories:
+- Source Code: https://github.com/caponetto/bayesian-hierarchical-clustering
+- Examples: https://github.com/caponetto/bayesian-hierarchical-clustering-examples
+
+---
+![Python application](https://github.com/caponetto/bhc/workflows/Python%20application/badge.svg) 
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![GitHub Stars](https://img.shields.io/github/stars/caponetto/bhc.svg)](https://github.com/caponetto/bhc/stargazers)
+[![GitHub Forks](https://img.shields.io/github/forks/caponetto/bhc.svg)](https://github.com/caponetto/bhc/network/members)
+
+# Bayesian Hierarchical Clustering in python
+This is a python implementation of the Bayesian Hierarchical Clustering algorithm proposed by Heller & Ghahramani (2005).
+> HELLER, Katherine A.; GHAHRAMANI, Zoubin. Bayesian hierarchical clustering. In: **Proceedings of the 22nd international conference on Machine learning**. 2005. p. 297-304.
+
+It also includes the Bayesian Rose Trees extension proposed by Blundell et al (2012).
+> BLUNDELL, Charles; TEH, Yee Whye; HELLER, Katherine A. Bayesian rose trees. arXiv preprint arXiv:1203.3468, 2012.
+
+## How to run the code
+1. Create an anaconda environment using the file [environment.yml](environment.yml).
+
+    `$ conda env create -f environment.yml`
+
+2. Activate the environment after the installation is completed.
+
+    `$ conda activate bayesian-hierarchical-clustering`
+
+3. Run the file [example.py](example.py).
+
+    `$ python example.py`
+
+4. Check out the output images in the [results](results/) folder.
+
+**Note**: You can optionally add your own data in the file [data.csv](data/data.csv) but the hyperparameters must be optimized.
+
+## What to expect from the example code
+A plot of the input data (2D). Suppose we want to find two clusters of data (orange and blue).
+
+<p align="center">
+  <a href="results/data_plot.png"><img src="results/data_plot.png" width="500"></a>
+</p>
+ 
+Dendrograms obtained from linkage algorithms. Notice that, none of them can tell us the presence of the two clusters.
+
+<p align="center">
+  <a href="results/linkage_single_plot.png"><img src="results/linkage_single_plot.png" width="250"></a> 
+  <a href="results/linkage_complete_plot.png"><img src="results/linkage_complete_plot.png" width="250"></a> 
+  <a href="results/linkage_average_plot.png"><img src="results/linkage_average_plot.png" width="250"></a>
+</p>
+
+A binary hierarchy obtained from the Bayesian hierarchical clustering algorithm. Notice that, two clusters have been identified containing the expected data points (leaves).
+
+<p align="center">
+  <a href="results/bhc_plot.png"><img src="results/bhc_plot.png" width="600"></a> 
+</p>
+
+A non-binary hierarchy obtained from the Bayesian rose trees algorithm. Notice that, two clusters have been identified containing the expected data points (leaves).
+
+<p align="center">
+  <a href="results/brt_plot.png"><img src="results/brt_plot.png" width="600"></a> 
+</p>
+
+## Contribute
+All contributions are welcome, so don't hesitate to submit a pull request. ;-)
+
+## Troubleshooting
+
+If you run into issues with graphviz when running the example, you might need to install it through
+
+`sudo apt-get install python-pydot python-pydot-ng graphviz`
+
+## License
+This code is released under GPL 3.0 License.
+
+Check [LICENSE](LICENSE) file for more information.
---
+++ example.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+
+# License: GPL 3.0
+
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+from graphviz import Digraph
+from scipy.cluster.hierarchy import dendrogram, linkage
+import pandas as pd
+
+
+from bhc import (BayesianHierarchicalClustering,
+                 BayesianRoseTrees,
+                 NormalInverseWishart)
+
+
+def main():
+    # dt = np.dtype([('col1', 'float64'), ('col2', 'float64'), ('col3', 'U50')])
+    # data = np.genfromtxt('data/data.csv', dtype=dt, delimiter=',')
+    data = np.genfromtxt('data/data.csv', delimiter=',')
+    # cities = np.genfromtxt('data/cities.csv', delimiter=',')
+    cities = pd.read_csv('data/cities.csv')
+    cities = [string for sublist in cities.values for string in sublist]
+    
+    # print(cities)
+    # print(data.shape)
+    # plot_data(data)
+
+    # run_linkage(data, 'single')
+    # run_linkage(data, 'complete')
+    # run_linkage(data, 'average', cities)
+
+    run_bhc(data)
+    # run_brt(data)
+
+
+def plot_data(data):
+        
+    colors = [  "#AB274F", "#7C0902", "#FE6F5E", "#BF4F51", "#FF00FF", "#00FFFF", "#800000", "#008000", "#000080",  "#FF8080", "#80FF80", "#8080FF", "#FFFF80", "#FF80FF", "#80FFFF", "#FF6666", "#66FF66", "#6666FF",  "#FFFF66", "#FF66FF", "#66FFFF", "#FF3333", "#33FF33", "#3333FF", "#FFFF33", "#FF33FF", "#33FFFF",  "#FF9999", "#99FF99", "#9999FF", "#FFFF99", "#FF99FF", "#99FFFF", "#FFCC00", "#CCFF00", "#00CCFF",  "#FF9966", "#66FF99", "#9966FF", "#FFFF66", "#FF66FF", "#66FFFF", "#FFCC99", "#99FFCC", "#CC99FF",  "#FFFFCC", "#CCFF66", "#66CCFF", "#FF99CC", "#CCFF99", "#99CCFF", "#FFCCCC", "#CCFFCC", "#CCCCFF",  "#FFFFFF", "#CCCCCC", "#999999", "#666666", "#333333", "#000000", "#F0F8FF", "#FAEBD7", "#00FFFF",  "#7FFFD4", "#F0FFFF", "#F5F5DC", "#FFE4C4", "#FFEBCD", "#000080", "#8A2BE2", "#A52A2A", "#DEB887",  "#5F9EA0", "#7FFF00", "#D2691E", "#FF7F50", "#6495ED", "#FFF8DC", "#DC143C", "#00FFFF", "#00008B",  "#008B8B", "#B8860B", "#A9A9A9", "#006400", "#BDB76B", "#8B008B", "#556B2F", "#FF8C00", "#9932CC",  "#8B0000", "#E9967A", "#8FBC8F", "#483D8B", "#2F4F4F", "#00CED1", "#9400D3", "#FF1493", "#00BFFF",  "#696969", "#1E90FF", "#B22222", "#FFFAF0", "#228B22", "#FF00FF", "#DCDCDC", "#F8F8FF", "#FFD700",  "#DAA520", "#808080", "#008000", "#ADFF2F", "#F0FFF0", "#FF69B4", "#CD5C5C", "#8B4513"]
+    
+    fig, ax = plt.subplots(figsize=(8, 6))
+    
+    for i in range(data.shape[0]):
+        x = data[i][0]
+        y = data[i][1]
+        label = data[i][2]
+        color = colors[i % len(colors)]
+        ax.scatter(x, y, c=color, label=label, s=100, alpha=0.7)
+        
+    ax.set_xlabel('PC 1', fontsize=14)
+    ax.set_ylabel('PC 2', fontsize=14)
+    ax.set_title('Scatter Plot', fontsize=16)
+    ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
+    
+    plt.savefig('results/scatter_plot.png', bbox_inches='tight')
+    plt.close()
+    
+    fig2, ax2 = plt.subplots(figsize=(8, 2))
+    handles, labels = ax.get_legend_handles_labels()
+    ax2.legend(handles, labels, loc='upper left', ncol=10, fontsize=8)
+    ax2.axis('off')
+    
+    # plt.savefig('results/legend.png', bbox_inches='tight')
+    # plt.close()
+
+
+
+
+def run_linkage(data, method, cities):
+    
+    frame = pd.DataFrame(data=data, columns=["PC 1", "PC 2"], index=cities)
+    plt.clf()
+    plt.figure(figsize=(15, 18))
+    Z = linkage(data, method)
+    dendrogram(Z, labels=frame.index, orientation = 'right')
+    
+    # plt.draw()
+    ax = plt.gca()
+    ax.tick_params(axis='x', which='major', labelsize=15)
+    ax.tick_params(axis='y', which='major', labelsize=8)
+    plt.savefig(
+        'results/linkage_{0}_plot.png'.format(method), format='png', dpi=100)
+
+
+def run_bhc(data):
+    # Hyper-parameters (these values must be optimized!)
+    g = 20
+    scale_factor = 0.001
+    alpha = 1
+
+    model = NormalInverseWishart.create(data, g, scale_factor)
+
+    bhc_result = BayesianHierarchicalClustering(data,
+                                                model,
+                                                alpha,
+                                                cut_allowed=True).build()
+
+    build_graph(bhc_result.node_ids,
+                bhc_result.arc_list,
+                'results/bhc_plot')
+
+
+def run_brt(data):
+    # Hyper-parameters (these values must be optimized!)
+    g = 10
+    scale_factor = 0.001
+    alpha = 0.5
+
+    model = NormalInverseWishart.create(data, g, scale_factor)
+
+    brt_result = BayesianRoseTrees(data,
+                                   model,
+                                   alpha,
+                                   cut_allowed=True).build()
+
+    build_graph(brt_result.node_ids,
+                brt_result.arc_list,
+                'results/brt_plot')
+
+
+def build_graph(node_ids, arc_list, filename):
+    dag = Digraph()
+
+    for id in node_ids:
+        dag.node(str(id))
+
+    for arc in arc_list:
+        dag.edge(str(arc.source), str(arc.target))
+
+    dag.render(filename=filename, format='png', cleanup=True)
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit()
